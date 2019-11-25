0325201902763b2760658ba7ba4e0d31e2beb924
Madeline Hightower <mhighto1@nd.edu>
2019-11-25 16:23:47 -0500

    Created Script File

---
+++ HightowerEx9.R
@@ -0,0 +1,16 @@
+# Madeline Hightower
+# Exercise 09
+
+# Part One
+# Load text file of two related variables from Excel or Text file
+# Produce a scatter plot of the two variables 
+
+# Part Two
+# Generate two figures summarizing the data in 'data.txt'
+
+
+# Barplot with the means of two populations
+
+# Generate a scatterplot with all of the observations
+# Use 'geom_jitter()' function to make observations easier to see
+# What do the bar versus scatter plots tell us?
